f8279923801903b04ae6e9090c3c8bff232b8a54
Tee Parham <tee@neighborland.com>
2014-04-07 12:57:11 -0600

    Compress css

--- config.rb
+++ config.rb
@@ -8,7 +8,8 @@ images_dir = "images"
 javascripts_dir = "./"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+# :expanded :nested :compact :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
